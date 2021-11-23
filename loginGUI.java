e5a53508c11ed1bcb1f5665308ea561d7c19610b
sam-india-007 <f20200021@pilani.bits-pilani.ac.in>
2021-11-23 14:59:20 +0530

    login signup fix

--- loginGUI.java
+++ loginGUI.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class loginGUI implements ActionListener{
+public class loginGUI{
 
     private static JLabel password1, label;
 	private static JTextField username;
@@ -19,7 +19,7 @@ public class loginGUI implements ActionListener{
         frame.setTitle("LOGIN PAGE");
         frame.setLocation(new Point(500, 300));
         frame.add(panel);
-        frame.setSize(new Dimension(400, 200));
+        frame.setSize(new Dimension(400, 300));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         label = new JLabel("Username");
@@ -42,40 +42,59 @@ public class loginGUI implements ActionListener{
         button.setBounds(100, 110, 90, 25);
         button.setForeground(Color.WHITE);
         button.setBackground(Color.BLACK);
-        button.addActionListener((ActionListener) new loginGUI());
+        LoginBtn l1 = new LoginBtn();
+        button.addActionListener(l1);
+        panel.add(button);
+
+        SignupBtn s1 = new SignupBtn();
+        button = new JButton("New User? Signup");
+        button.setBounds(100, 150, 175, 25);
+        button.setForeground(Color.WHITE);
+        button.setBackground(Color.BLACK);
+        button.addActionListener(s1);
         panel.add(button);
 
         frame.setVisible(true);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String Username = username.getText();
-        String Password1 = String.valueOf(Password.getPassword());
+    //@Override
+    private static class LoginBtn implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            String Username = username.getText();
+            String Password1 = String.valueOf(Password.getPassword());
 
-        // below this function needs to change
-        // if (Username.equals("section.io") && Password1.equals("123"))
-        //     JOptionPane.showMessageDialog(null, "Login Successful");
-        // else
-        //     JOptionPane.showMessageDialog(null, "Username or Password mismatch ");
-        // changed function
-        try{
-            int status = Player.login(Username, Password1);
-            if(status > 0){
-                Player current_player = Player.get_current_player_details(status);
-                JOptionPane.showMessageDialog(null, "You are logged in with id :" + current_player.id);
-                homeGUI.createUI(current_player);
-                frame.setVisible(false);
+            // below this function needs to change
+            // if (Username.equals("section.io") && Password1.equals("123"))
+            //     JOptionPane.showMessageDialog(null, "Login Successful");
+            // else
+            //     JOptionPane.showMessageDialog(null, "Username or Password mismatch ");
+            // changed function
+            try{
+                int status = Player.login(Username, Password1);
+                if(status > 0){
+                    Player current_player = Player.get_current_player_details(status);
+                    JOptionPane.showMessageDialog(null, "You are logged in with id :" + current_player.id);
+                    homeGUI.createUI(current_player);
+                    frame.setVisible(false);
+                }
+                else{
+                    JOptionPane.showMessageDialog(null, "Check your username and password again !");
+                }
             }
-            else{
-                JOptionPane.showMessageDialog(null, "Check your username and password again !");
+            catch(Exception exp){
+                exp.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Error in logging in player !");
             }
-        }
-        catch(Exception exp){
-            exp.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Error in logging in player !");
         }
     }
+
+    private static class SignupBtn implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            signupGUI.createUI();
+            frame.setVisible(false);
+        }
+    }
+    
 
 
 }
